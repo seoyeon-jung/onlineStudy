145b5b9f4f69c264408f41e05fd3e81c0bd3a0d9
seoyeon-jung <jungluchia1204@gmail.com>
2022-01-18 16:09:37 +0900

    7일차 과제

---
+++ src/work1/Work7_2.java
@@ -0,0 +1,14 @@
+package work1;
+
+public class Work7_2 {
+
+	public static void main(String[] args) {
+		
+		int a = 3;
+		int b = 5;
+		
+		System.out.printf("%d %d %d", ++a, b--, a, ++b);
+
+	}
+
+}
